8dd645e5e31e5819da4a1bd51ca64fe7ba6268ef
KRINNA PATEL <KRINNA415@GMAIL.COM>
2025-02-14 15:47:40 -0500

    ModalWithForm

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,5 +1,22 @@
-function ModalWithForm() {
-  return;
+import "./ModalWithForm.css";
+
+function ModalWithForm({ children, buttonText, title }) {
+  return (
+    <div className="modal">
+      <div className="modal__content">
+        <h2 className="modal__title">New Garment</h2>
+        <button type="button" className="modal__close">
+          Close
+        </button>
+        <form className="modal__form">
+          {children}
+          <button type="submit" className="modal__submit">
+            {buttonText}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default ModalWithForm;
